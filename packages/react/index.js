949281235b0af14114ef778de8f2a45e881988b9
いーちゃん <echan041007@gmail.com>
2022-04-04 19:48:21 +0900

    chore(react): ルールがrulesに入っていなかった

--- packages/react/index.js
+++ packages/react/index.js
@@ -1,5 +1,7 @@
 module.exports = {
   "extends": ["eslint:recommended", "@e-chan1007/base", "plugin:react/recommended"],
-  "jsx-quotes": ["error", "prefer-double"],
-  "react/jsx-indent": ["error", 2]
+  "rules": {
+    "jsx-quotes": ["error", "prefer-double"],
+    "react/jsx-indent": ["error", 2]
+  }
 };
