3c02fb6c0ac5eb7e19447a2ecee1442c4443f84a
いーちゃん <echan041007@gmail.com>
2022-04-04 18:22:39 +0900

    feat(vue): Vueの設定を書く

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -1,1 +1,9 @@
-module.exports = {};
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "@e-chan1007/base",
+    "plugin:vue/vue3-essential",
+    "@vue/eslint-config-typescript/recommended"
+  ],
+  "rules": { "vue/html-closing-bracket-newline": ["error", { "singleline": "never", "multiline": "never" }] }
+};
