bd8340395172b9577d2bd8c14ced8d3d66ddac9c
いーちゃん <echan041007@gmail.com>
2022-04-04 18:13:58 +0900

    feat(react): Reactの設定を書く

---
+++ packages/react/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "extends": ["eslint:recommended", "@e-chan1007/base", "plugin:react/recommended"],
+  "jsx-quotes": ["error", "prefer-double"],
+  "react/jsx-indent": ["error", 2]
+};
