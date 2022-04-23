5152495e409ef013f45feab070fad77ec707e08d
いーちゃん <echan041007@gmail.com>
2022-04-23 21:38:52 +0900

    chore(vue): インデントとコンポーネント名のルール追加

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -5,5 +5,15 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "@vue/typescript/recommended"
   ],
-  "rules": { "vue/html-closing-bracket-newline": ["error", { "singleline": "never", "multiline": "never" }] }
+  "rules": {
+    "vue/html-closing-bracket-newline": ["error", { "singleline": "never", "multiline": "never" }],
+    "vue/html-indent": ["error", 2, {
+      "attribute": 1,
+      "baseIndent": 1,
+      "closeBracket": 0,
+      "alignAttributesVertically": true,
+      "ignores": []
+    }],
+    "vue/multi-word-component-names": "off"
+  }
 };
